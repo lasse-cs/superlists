b8b18d99eb522ff4f2790c3b53b7caaf3bc63c8c
lasse-cs <lasse.schmieding@gmail.com>
2025-11-05 23:40:23 +0000

    First unit test and view function

--- lists/tests.py
+++ lists/tests.py
@@ -1,2 +1,11 @@
-def test_bad_maths():
-    assert 1 + 1 == 3
+from django.http import HttpRequest
+from lists.views import home_page
+
+
+def test_home_page_returns_correct_html():
+    request = HttpRequest()
+    response = home_page(request)
+    html = response.content.decode("utf8")
+    assert "<title>To-Do lists</title>" in html
+    assert html.startswith("<html>")
+    assert html.endswith("</html>")
