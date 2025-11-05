43158c85fbc73f971a40eef44da513d87da05077
lasse-cs <lasse.schmieding@gmail.com>
2025-11-05 23:47:55 +0000

    url config, map / to home_page view

--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,11 @@
 from django.http import HttpRequest
+from pytest_django.asserts import assertContains 
+
 from lists.views import home_page
 
 
-def test_home_page_returns_correct_html():
-    request = HttpRequest()
-    response = home_page(request)
-    html = response.content.decode("utf8")
-    assert "<title>To-Do lists</title>" in html
-    assert html.startswith("<html>")
-    assert html.endswith("</html>")
+def test_home_page_returns_correct_html(client):
+    response = client.get("/")
+    assertContains(response, "<title>To-Do lists</title>")
+    assertContains(response, "<html>")
+    assertContains(response, "</html>")
