129984c56b9d27e7c5cdef8168abd5def42c11fd
lasse-cs <lasse.schmieding@gmail.com>
2025-11-06 06:58:25 +0000

    Refactor test to use the template

--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,14 @@
 from django.http import HttpRequest
-from pytest_django.asserts import assertContains 
+from pytest_django.asserts import assertContains, assertTemplateUsed
 
 from lists.views import home_page
 
 
 def test_home_page_returns_correct_html(client):
     response = client.get("/")
-    assertContains(response, "<title>To-Do lists</title>")
-    assertContains(response, "<html>")
-    assertContains(response, "</html>")
+    assertTemplateUsed(response, "home.html")
+
+
+def test_renders_homepage_content(client):
+    response = client.get("/")
+    assertContains(response, "To-Do")
