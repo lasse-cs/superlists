1f6bf7053c1ab8e68838c3f1b1e8757046f01573
lasse-cs <lasse.schmieding@gmail.com>
2025-11-06 07:25:22 +0000

    Model for list items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,9 @@
+import pytest
+
 from django.http import HttpRequest
 from pytest_django.asserts import assertContains, assertTemplateUsed
 
+from lists.models import Item
 from lists.views import home_page
 
 
@@ -19,3 +22,23 @@ def test_can_save_a_POST_request(client):
     response = client.post("/", data={"item_text": "A new list item"})
     assertContains(response, "A new list item")
     assertTemplateUsed(response, "home.html")
+
+
+@pytest.mark.django_db
+def test_saving_and_retrieving_items():
+    first_item = Item()
+    first_item.text = "The first (ever) list item"
+    first_item.save()
+
+    second_item = Item()
+    second_item.text = "Item the second"
+    second_item.save()
+
+    saved_items = Item.objects.all()
+    assert saved_items.count() == 2
+
+    first_saved_item = saved_items[0]
+    second_saved_item = saved_items[1]
+
+    assert first_saved_item.text == "The first (ever) list item"
+    assert second_saved_item.text == "Item the second"
