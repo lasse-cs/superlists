d12ec4fc13f093605fc3ff40a25d9e92cd266a73
lasse-cs <lasse.schmieding@gmail.com>
2025-11-07 06:31:58 +0000

    Placeholders for using form in view+template, not in use yet

--- src/lists/views.py
+++ src/lists/views.py
@@ -2,10 +2,12 @@ from django.core.exceptions import ValidationError
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 
 def home_page(request: HttpRequest) -> HttpResponse:
+    # return render(request, "home.html", {"form": ItemForm()})
     return render(request, "home.html")
 
 
