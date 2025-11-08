165ef4c623e3637a0d3ad1b69f50c9b173f8ea9e
lasse-cs <lasse.schmieding@gmail.com>
2025-11-08 10:00:35 +0000

    Despike JS, add vitest tests

---
+++ src/lists/static/lists.js
@@ -0,0 +1,7 @@
+export const initialize = (inputSelector, errorSelector) => {
+    const textInput = document.querySelector("#id_text");
+    textInput.oninput = () => {
+        const errorMsg = document.querySelector(".invalid-feedback");
+        errorMsg.style.display = "none";
+    };
+};
---
+++ src/lists/static/tests/lists.test.js
@@ -0,0 +1,48 @@
+import '@testing-library/jest-dom';
+
+import { initialize } from '../lists.js';
+
+describe("Superlists JavaScript", () => {
+    const inputId = "id_text";
+    const errorClass = "invalid-feedback";
+    const inputSelector = `#${inputId}`;
+    const errorSelector = `.${errorClass}`;
+    let testDiv;
+    let textInput;
+    let errorMsg;
+
+    beforeEach(() => {
+        testDiv = document.createElement("div");
+        testDiv.innerHTML = `
+            <form>
+                <input
+                    id="${inputId}"
+                    name="text"
+                    class="form-control form-control-lg is-invalid"
+                    placeholder="Enter a to-do item"
+                    value="Value as submitted"
+                    aria-describedby="id_text_feedback"
+                    required
+                >
+                <div id="id_text_feedback" class="${errorClass}">An error message</div>
+            </form>
+        `;
+        document.body.appendChild(testDiv);
+        textInput = document.querySelector(inputSelector);
+        errorMsg = document.querySelector(errorSelector);
+        initialize(inputSelector, errorSelector);
+    });
+
+    afterEach(() => {
+       testDiv.remove(); 
+    });
+
+    it("should hide error message on input", async () => {
+        textInput.dispatchEvent(new InputEvent("input"));
+        expect(errorMsg).not.toBeVisible();
+    });
+
+    it("should not hide error message before event is fired", () => {
+        expect(errorMsg).toBeVisible() 
+    });
+});
