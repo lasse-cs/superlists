3b436b8c8ed96ebd9fee438ae318e99b9f2a1595
lasse-cs <lasse.schmieding@gmail.com>
2025-11-10 21:25:33 +0000

    Fix formatting with ruff

--- src/accounts/tests/test_authentication.py
+++ src/accounts/tests/test_authentication.py
@@ -11,7 +11,8 @@ pytestmark = pytest.mark.django_db
 
 def test_authenticate_returns_none_if_no_such_token():
     result = PasswordlessAuthenticationBackend().authenticate(
-        HttpRequest(), "no-such-token",
+        HttpRequest(),
+        "no-such-token",
     )
     assert result is None
 
@@ -19,9 +20,7 @@ def test_authenticate_returns_none_if_no_such_token():
 def test_authenticate_returns_new_user_with_correct_email_if_token_exists():
     email = "edith@example.com"
     token = Token.objects.create(email=email)
-    user = PasswordlessAuthenticationBackend().authenticate(
-        HttpRequest(), token.uid 
-    )
+    user = PasswordlessAuthenticationBackend().authenticate(HttpRequest(), token.uid)
     new_user = User.objects.get(email=email)
     assert user == new_user
 
@@ -30,9 +29,7 @@ def test_authenticate_returns_existing_user_with_correct_email_if_token_exists()
     email = "edith@example.com"
     existing_user = User.objects.create(email=email)
     token = Token.objects.create(email=email)
-    user = PasswordlessAuthenticationBackend().authenticate(
-        HttpRequest(), token.uid
-    )
+    user = PasswordlessAuthenticationBackend().authenticate(HttpRequest(), token.uid)
     assert user == existing_user
 
 
@@ -41,6 +38,7 @@ def test_get_user_by_email():
     desired_user = User.objects.create(email="edith@example.com")
     found_user = PasswordlessAuthenticationBackend().get_user("edith@example.com")
     assert found_user == desired_user
+
 
 def test_get_user_returns_None_if_no_user_with_that_email():
     assert PasswordlessAuthenticationBackend().get_user("edith@example.com") is None
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -11,9 +11,7 @@ from accounts.models import Token
 def send_login_email(request):
     email = request.POST["email"]
     token = Token.objects.create(email=email)
-    url = request.build_absolute_uri(
-        reverse("login") + "?token=" + str(token.uid)
-    )
+    url = request.build_absolute_uri(reverse("login") + "?token=" + str(token.uid))
     send_mail(
         "Your login link for Superlists",
         f"Use this link to log in:\n\n{url}",
